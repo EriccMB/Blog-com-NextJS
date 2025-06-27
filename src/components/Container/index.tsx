a9c09378fa30a932d9ae06863ce399370aacd0bb
Eric Morais <ericmorais198@gmail.com>
2025-06-27 12:16:22 -0300

    .

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -6,6 +6,7 @@ export function Container({ children }: ContainerProps) {
   return (
     <div className="min-h-screen bg-stone-100 text-stone-900 dark:bg-stone-900 dark:text-stone-100">
       <div className="max-w-screen-lg mx-auto  px-8">{children}</div>
+         <div className="max-w-screen-lg mx-auto  px-8">{children}</div>
     </div>
   );
 }
