dc030ff8aa6f5da400807c4ff22d9dc3cece8936
Eric Morais <ericmorais198@gmail.com>
2025-09-10 22:12:13 -0300

    configuração do drizzle orm - sqlite

---
+++ src/db/drizzle/schemas.ts
@@ -0,0 +1,18 @@
+import { InferInsertModel, InferSelectModel } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+export const PostTable = sqliteTable('post', {
+  id: text('id').primaryKey(),
+  slug: text('slug').notNull().unique(),
+  title: text('title').notNull(),
+  author: text('author').notNull(),
+  excerpt: text('excerpt').notNull(),
+  content: text('content').notNull(),
+  coverImageUrl: text('coverImageUrl').notNull(),
+  published: integer('published', { mode: 'boolean' }).notNull(),
+  createdAt: text('createdAt').notNull(),
+  updatedAt: text('updatedAt').notNull(),
+});
+
+export type PostTableSelectMode = InferSelectModel<typeof PostTable>;
+export type PostTableInsertMode = InferInsertModel<typeof PostTable>;
