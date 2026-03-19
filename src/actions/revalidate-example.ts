a8efeffb2dc5b945da0945e216dc92249db85463
Eric Morais <ericmorais198@gmail.com>
2026-03-19 14:45:02 -0300

    .

--- src/actions/revalidate-example.ts
+++ src/actions/revalidate-example.ts
@@ -1,12 +1,12 @@
 'use server';
 
-import { revalidatePath, revalidateTag } from 'next/cache';
+import { revalidateTag } from 'next/cache';
 
 export async function revalidateExampleAction(formData: FormData) {
   const path = formData.get('path') || '';
   console.log('server actions chamada', path);
 
   // revalidatePath(`${path}`);
-  revalidateTag('posts');
-  revalidateTag('post-rotina-matinal-de-pessoas-altamente-eficazes');
+  revalidateTag('posts'); // home
+  revalidateTag('post-rotina-matinal-de-pessoas-altamente-eficazes'); // um post apenas
 }
